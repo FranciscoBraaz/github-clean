0004d238d4c501a6efdf88616a4b8f8fc1978507
FranciscoBraaz <francisco_braaz@hotmail.com>
2022-03-19 12:24:44 -0300

    feat(AuthContext): configurando contexto de autenticação

--- App.tsx
+++ App.tsx
@@ -1,12 +1,18 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from '@react-navigation/native';
+import { AuthProvider } from './src/contexts/AuthContext';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <AuthProvider>
+        <View style={styles.container}>
+          <Text>Open up App.tsx to start working on your app!</Text>
+          <StatusBar style="auto" />
+        </View>
+      </AuthProvider>
+    </NavigationContainer>
   );
 }
 
---
+++ src/contexts/AuthContext.tsx
@@ -0,0 +1,29 @@
+import React, { createContext, useContext } from 'react';
+
+// interface userData {}
+
+interface AuthContextData {
+  isAuthenticated: boolean;
+}
+
+type AuthProviderType = {
+  children: React.ReactNode;
+};
+
+const AuthContext = createContext({} as AuthContextData);
+
+export function AuthProvider({ children }: AuthProviderType) {
+  const isAuthenticated = false;
+
+  return (
+    <AuthContext.Provider value={{ isAuthenticated }}>
+      {children}
+    </AuthContext.Provider>
+  );
+}
+
+export function useAuth() {
+  const context = useContext(AuthContext);
+
+  return context;
+}
